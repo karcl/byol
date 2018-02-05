373766abd13193343d752ac099e0d18940af8169
kc <flataux@gmail.com>
2018-02-05 21:53:49 +0100

    Chapter 4: echo REPL

--- src/main.c
+++ src/main.c
@@ -1,7 +1,27 @@
 #include <stdio.h>
 
+#define VERSION "0.0.0.0.1"
+
+/* User input buffer */
+#define BUFFER_SIZE 2048
+static char input[BUFFER_SIZE];
+
 int main(int argc, char* argv[])
 {
-  printf("This is a test\n");
+  fprintf(stdout, "Byol Version %s\n", VERSION);
+  fprintf(stdout, "Press Ctrl+d to Exit\n");
+
+  while (1) {
+    fprintf(stdout, "byol> "); /* Show prompt */
+
+    fgets(input, BUFFER_SIZE, stdin); /* Get user input */
+
+    fprintf(stdout, "You've entered: %s", input); /* Echo input back */
+
+    if (feof(stdin)) { /* Terminate loop on EOF */
+      break;
+    }
+  }
+
   return 0;
 }
